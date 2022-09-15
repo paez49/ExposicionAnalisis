fe96c27776788b34e2fcb9eefdf0d33cc507b423
Zayber <zayber.re@gmail.com>
2022-09-15 15:28:13 -0500

    Changes

--- Codigo/muller_method.py
+++ Codigo/muller_method.py
@@ -2,7 +2,14 @@
 from sympy import sympify, Symbol, lambdify, SympifyError
 from cmath import sqrt
 import readline
-    
+
+def secantMethod(f, x0, x1, error, iterations):
+    for i in range(iterations):
+        x2 = x1 - f(x1) * (x1 - x0) / float(f(x1) - f(x0))
+        if abs(x2 - x1) < error:
+            return i, x2
+        x0, x1 = x1, x2
+    return i, x2
 
 def mullerMethod(f, x0, x1, x2, error, max_iter):
     for times in range(max_iter):
@@ -20,7 +27,7 @@ def mullerMethod(f, x0, x1, x2, error, max_iter):
         #Hallar x3
         x3 = x2 + (-2 * c)/deno
         if abs(x3 - x2) < error:
-            return max_iter, x3
+            return times, x3
         x0, x1, x2 = x1, x2, x3
 
     return times, x3
@@ -33,12 +40,14 @@ def main():
         error = input("Error (default: 1e-5): ")
         error = 1e-5 if error == "" else float(error)
         iterations = input("Número max de iteraciones (dafault: 100): ")
-        iterations = 100 if iterations == "" else int(itereations)
+        iterations = 100 if iterations == "" else int(iterations)
         x0 = float(input("x0: "))
         x1 = float(input("x1: "))
         x2 = float(input("x2: "))
         times, root = mullerMethod(f, x0, x1, x2, error, iterations)
-        print(f"Raiz: {root}, con {times} iteraciones")
+        print(f"Raiz: {root}, con {times+1} iteraciones")
+        times, root = secantMethod(f, x0, x1, error, iterations)
+        print(f"Raiz: {root}, con {times+1} iteraciones (método de la secante)")
     except SympifyError as e:
         print(f"Error parsing string: {e.expr}")
     except Exception as e:
